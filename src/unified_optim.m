1525711f6f3630a690d24f0fc454b773b8514bb6
Yuan <yuan.tian277@duke.edu>
2020-10-17 20:15:11 -0400

    add blur, downsample, and adding noise function

--- src/unified_optim.m
+++ src/unified_optim.m
@@ -1,10 +1,19 @@
 close all;
 clear;
 
+% Get raw image
 video_name = "../data/foreman_qcif.y4m";
 [mov_color_struct, mov_info] = yuv4mpeg2mov(video_name);
 num_frame = 30;
 mov_raw = preprocess_video(mov_color_struct, num_frame);
+
+% Blur, downsample, and add noise
+blur_kernel = fspecial('gaussian', [7 7], 1.5);
+dp_factor = 4;
+gaussian_noise_std = 1;
+mov_lr = gen_lr_video(mov_raw, blur_kernel, dp_factor, gaussian_noise_std);
+
+
 
 
 function mov_raw = preprocess_video(mov_color_struct, num_frame)
@@ -13,4 +22,11 @@ function mov_raw = preprocess_video(mov_color_struct, num_frame)
     for i = 1:num_frame
         mov_raw(:, :, i) = rgb2gray(mov_color_struct(i).cdata);
     end
+end
+
+function mov_lr = gen_lr_video(mov_raw, blur_kernel, dp_factor, noise_std)
+    mov_blur = imfilter(mov_raw, blur_kernel, 'symmetric', 'same', 'conv');
+    mov_dp = mov_blur(1:dp_factor:end, 1:dp_factor:end, :);
+    mov_lr = rescale(imnoise(rescale(mov_dp), 'gaussian', 0, ...
+        noise_std/255.0), 0, 255);
 end
